bf86dd5526e9561b23f965ebce57e3fb4a9aee9e
Sharath <sharath.esr@gmail.com>
2025-02-20 21:10:27 +0530

    feat: implement protected route for cart page

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import {HomePage, Login, ProductsList, Register,CartPage} from '../pages'
 import { ProductDetail } from '../pages/ProductDetail'
+import { ProtectedRoute } from './ProtectedRoute'
 
 export const AllRoutes =() =>  {
   return (
@@ -12,7 +13,7 @@ export const AllRoutes =() =>  {
 
          <Route path="login" element={<Login/>}/>
          <Route path="register" element={<Register/>}/>
-         <Route path="cart" element={<CartPage/>}/>
+         <Route path="cart" element={<ProtectedRoute><CartPage/></ProtectedRoute>}/>
     </Routes>
   )
 }

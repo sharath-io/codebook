750104ee5ba154e43d1ea8bd793d562a4bfc1ef3
Sharath <sharath.esr@gmail.com>
2025-02-22 10:07:01 +0530

    feat: add read guard for product API calls

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,17 +1,17 @@
 export const getProductList = async (searchTerm) => {
-  const response = await fetch(`http://localhost:8000/products?name_like=${searchTerm ? searchTerm : ""}`);
+  const response = await fetch(`http://localhost:8000/444/products?name_like=${searchTerm ? searchTerm : ""}`);
   const data = await response.json();
   return data;
 };
 
 export const getProduct = async (id) => {
-  const response = await fetch(`http://localhost:8000/products/${id}`);
+  const response = await fetch(`http://localhost:8000/444/products/${id}`);
   const data = await response.json();
   return data;
 };
 
 export const getFeaturedList = async () => {
-  const response = await fetch("http://localhost:8000/featured_products");
+  const response = await fetch("http://localhost:8000/444/featured_products");
   const data = await response.json();
   return data;
 };
